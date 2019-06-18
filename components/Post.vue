7ce5e728bed989f2862535da438c15178335d5f8
Mahmoud Shiref <Sniffabyte@gmail.com>
2019-06-18 13:30:49 +0200

    Linking

--- components/Post.vue
+++ components/Post.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="post">
-        <h1>{{ post.title }}</h1>
+        <h1>
+            <nuxt-link :to="{ name: 'posts-id', params: { id: post.id } }">{{ post.title }}</nuxt-link>
+        </h1>
         <p>{{ post.body }}</p>
     </div>
 </template>
