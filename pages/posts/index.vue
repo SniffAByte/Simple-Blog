e1bd1967902e95fb4cf933be9111912447c68f7a
Mahmoud Shiref <Sniffabyte@gmail.com>
2019-06-18 12:44:35 +0200

    Posts Page

---
+++ pages/posts/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>{{ posts }}</p>   
+    </div>    
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            posts: []
+        }
+    },
+    async asyncData() {
+        let res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        return {
+            posts: res.data
+        }
+    }
+}
+</script>
+
